9284197d9c5c458a42f64089ea90327122aa8fa1
EbrahimMohamud <ebrahim.mohamud@mail.utoronto.ca>
2025-05-14 13:12:13 -0400

    SEYAHHHH

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         FizzBuzz.doFizzBuzz(15);
         FizzBuzz.main(args);
-        Weaintmissedthemill = 1000000;
-        Seeeyyaaaa = "SYEAAAAAA";
+        int seyah = 1000000;
+        int feweh = 1100000;
+
     }
 }
